b8326703def855bd35d5795f5509560f05559f6c
blondyn <kucharzyk.miroslaw@gmail.com>
2017-11-13 20:06:36 +0100

    remove excess children

--- src/patch.js
+++ src/patch.js
@@ -1,8 +1,41 @@
 module.exports = patch;
 
 function patch ({ parent, newVirtualNode, element, oldVirtualNode }) {
+    if (oldVirtualNode) {
+        removeExcessiveChildren(oldVirtualNode, newVirtualNode, element);
+        if (oldVirtualNode.tag == newVirtualNode.tag) {
+            return updateElementData(newVirtualNode, oldVirtualNode, element);
+        } else {
+            const newElement = createElement(newVirtualNode);
+            parent.replaceChild(newElement, element);
+            return newElement
+        }
+    }
     const newElement = createElement(newVirtualNode);
     return parent.appendChild(newElement);
+};
+
+function updateElementData (newVirtualNode, oldVirtualNode, element) {
+    for (let attr in newVirtualNode.data) {
+        if (newVirtualNode.data[attr] !== oldVirtualNode.data[attr]) {
+            element.setAttribute(attr, newVirtualNode.data[attr]);
+        }
+    }
+
+    for (let attr in oldVirtualNode.data) {
+        if (newVirtualNode.data[attr] == null) {
+            element.removeAttribute(attr);
+        }
+    }
+    return element;
+};
+
+function removeExcessiveChildren (oldVirtualNode, newVirtualNode, element) {
+    const childrenToRemove = [];
+    for (let i = newVirtualNode.children.length; i < oldVirtualNode.children.length; ++i) {
+        childrenToRemove.push(element.childNodes[i]);
+    }
+    childrenToRemove.forEach(child => element.removeChild(child));
 };
 
 function createElement (newVirtualNode) {
@@ -23,4 +56,4 @@ function createTagElement (newVirtualNode) {
         patch({ parent: newElement, newVirtualNode: child });
     });
     return newElement;
-}
+};
--- test/patch.test.js
+++ test/patch.test.js
@@ -49,3 +49,101 @@ test("create new element with children elements", t => {
 
     t.end();
 });
+
+test("replace element with a new one", t => {
+    document.body.innerHTML = "<div></div>";
+    const newElement = patch({
+        parent: document.body,
+        newVirtualNode: h("span"),
+        element: document.querySelector("div"),
+        oldVirtualNode: h("div"),
+    });
+
+    t.strictEqual(document.body.innerHTML, "<span></span>");
+    t.strictEqual(newElement.outerHTML, "<span></span>");
+
+    t.end();
+});
+
+
+test("update element attributes", t => {
+    document.body.innerHTML = '<body><div id="foo"></div></body>';
+    const oldElement = document.querySelector("div");
+    const newElement = patch({
+        parent: document.body,
+        element: document.querySelector("div"),
+        oldVirtualNode: h("div", {id: 'foo'}),
+        newVirtualNode: h("div", {id: "bar", class: "baz"})
+    });
+    t.strictEqual(document.body.innerHTML, '<div id="bar" class="baz"></div>');
+    t.strictEqual(newElement.outerHTML, '<div id="bar" class="baz"></div>');
+    t.strictEqual(newElement, oldElement);
+
+    t.end();
+});
+
+test("should not update element attributes, when those are not changed", t => {
+    document.body.innerHTML = '<body><div id="foo"></div></body>';
+    const oldElement = document.querySelector("div");
+    oldElement.setAttribute = () => {throw new Error("should not be called")};
+
+    const newElement = patch({
+        parent: document.body,
+        element: oldElement,
+        oldVirtualNode: h("div", {id: 'foo'}),
+        newVirtualNode: h("div", {id: "foo"})
+    });
+    t.strictEqual(document.body.innerHTML, '<div id="foo"></div>');
+    t.strictEqual(newElement.outerHTML, '<div id="foo"></div>');
+    t.strictEqual(newElement, oldElement);
+
+    t.end();
+});
+
+test("should remove element attributes, when those are not changed", t => {
+    document.body.innerHTML = '<body><div id="foo"></div></body>';
+    const oldElement = document.querySelector("div");
+    const newElement = patch({
+        parent: document.body,
+        element: oldElement,
+        oldVirtualNode: h("div", {id: 'foo'}),
+        newVirtualNode: h("div", {})
+    });
+    t.strictEqual(document.body.innerHTML, '<div></div>');
+    t.strictEqual(newElement.outerHTML, '<div></div>');
+    t.strictEqual(newElement, oldElement);
+
+    t.end();
+});
+
+test("should remove excess child nodes", t => {
+    document.body.innerHTML = '<body><div><span></span></div></body>';
+    const oldElement = document.querySelector("div");
+    const newElement = patch({
+        parent: document.body,
+        element: oldElement,
+        oldVirtualNode: h("div", {}, [h("span")]),
+        newVirtualNode: h("div")
+    });
+    t.strictEqual(document.body.innerHTML, '<div></div>');
+    t.strictEqual(newElement.outerHTML, '<div></div>');
+
+    t.end();
+});
+//
+// test("patch matching children on update", t => {
+//     document.body.innerHTML = '<div><span id="foo"></span></div>';
+//     const oldChild = document.querySelector("span");
+//     patch({
+//         parent: document.body,
+//         element: document.querySelector("div"),
+//         oldVirtualNode: h("div", {}, [h("span", {id: "foo"})]),
+//         newVirtualNode: h("div", {}, [h("span", {id: "bar"})])
+//     });
+//     const newChild = document.querySelector("span");
+//
+//     t.strictEqual(document.body.innerHTML, '<div><span id="bar"></span></div>');
+//     t.strictEqual(oldChild, newChild);
+//
+//     t.end();
+// });
