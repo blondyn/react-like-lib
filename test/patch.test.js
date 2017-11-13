277a2f07a799e0063fc316396e592952f55da97c
blondyn <kucharzyk.miroslaw@gmail.com>
2017-11-13 19:06:33 +0100

    testing nesting

--- test/patch.test.js
+++ test/patch.test.js
@@ -5,7 +5,7 @@ require("jsdom-global")();
 
 test("create new element", t => {
     document.body.innerHTML = "";
-    const newElement = patch({parent: document.body, newVirtualNode: h("div", {id: "foo"})});
+    const newElement = patch({ parent: document.body, newVirtualNode: h("div", { id: "foo" }) });
 
     t.strictEqual(document.body.innerHTML, '<div id="foo"></div>');
     t.strictEqual(newElement.outerHTML, '<div id="foo"></div>');
@@ -15,7 +15,7 @@ test("create new element", t => {
 
 test("create new text element", t => {
     document.body.innerHTML = "";
-    const newElement = patch({parent: document.body, newVirtualNode: "some text"});
+    const newElement = patch({ parent: document.body, newVirtualNode: "some text" });
 
     t.strictEqual(document.body.innerHTML, "some text");
     t.strictEqual(newElement.textContent, "some text");
@@ -23,12 +23,29 @@ test("create new text element", t => {
     t.end();
 });
 
-test("create new element with children assigned", t => {
+test("create new element with a text child", t => {
     document.body.innerHTML = "";
-    const newElement = patch({parent: document.body, newVirtualNode: h("div", {id: 'foo'}, ["some text"])});
+    const newElement = patch({ parent: document.body, newVirtualNode: h("div", { id: 'foo' }, ["some text"]) });
 
     t.strictEqual(document.body.innerHTML, '<div id="foo">some text</div>');
     t.strictEqual(newElement.outerHTML, '<div id="foo">some text</div>');
+
+    t.end();
+});
+
+test("create new element with children elements", t => {
+    document.body.innerHTML = "";
+    const newElement = patch({
+        parent: document.body,
+        newVirtualNode: h("div", { id: 'foo' }, [
+            "some text",
+            h("p", { id: 'paragraph' },
+                ["some text 2"])
+        ])
+    });
+
+    t.strictEqual(document.body.innerHTML, '<div id="foo">some text<p id="paragraph">some text 2</p></div>');
+    t.strictEqual(newElement.outerHTML, '<div id="foo">some text<p id="paragraph">some text 2</p></div>');
 
     t.end();
 });
