b6e41dfb8c8c51645b6e8c3d98fdcd40cddcc17c
blondyn <kucharzyk.miroslaw@gmail.com>
2017-11-13 19:20:30 +0100

    a bit of refactoring

--- src/patch.js
+++ src/patch.js
@@ -1,17 +1,26 @@
-module.exports = function patch ({ parent, newVirtualNode }) {
-    let newElement;
-    if (typeof newVirtualNode === 'string') {
-        newElement = document.createTextNode(newVirtualNode)
-    } else {
-        newElement = document.createElement(newVirtualNode.tag);
+module.exports = patch;
 
-        Object.keys(newVirtualNode.data).forEach((key) => {
-            newElement.setAttribute(`${key}`, newVirtualNode.data[key])
-        });
-
-        newVirtualNode.children.forEach(child => {
-            patch({parent: newElement, newVirtualNode: child});
-        });
-    }
+function patch ({ parent, newVirtualNode, element, oldVirtualNode }) {
+    const newElement = createElement(newVirtualNode);
     return parent.appendChild(newElement);
 };
+
+function createElement (newVirtualNode) {
+    if (typeof newVirtualNode === 'string') {
+        return document.createTextNode(newVirtualNode)
+    }
+    return createTagElement(newVirtualNode);
+};
+
+function createTagElement (newVirtualNode) {
+    const newElement = document.createElement(newVirtualNode.tag);
+
+    Object.keys(newVirtualNode.data).forEach((key) => {
+        newElement.setAttribute(`${key}`, newVirtualNode.data[key])
+    });
+
+    newVirtualNode.children.forEach(child => {
+        patch({ parent: newElement, newVirtualNode: child });
+    });
+    return newElement;
+}
